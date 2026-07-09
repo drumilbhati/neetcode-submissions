c7a3835309b9a294afadbfa7d1e01bf138526e1f
Drumil Bhati <64512394+drumilbhati@users.noreply.github.com>
2026-07-09 07:20:01 +0530

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<int> ans;
+        multiset<int> ms;
+
+        for (int i=0; i<n; i++) {
+            ms.insert(nums[i]);
+
+            if (i >= k) {
+                ms.erase(ms.find(nums[i-k]));
+            }
+
+            if (i >= k-1) {
+                ans.push_back(*ms.rbegin());
+            }
+        }
+        return ans;
+    }
+};
