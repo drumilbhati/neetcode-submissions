4e3b6c26f0b4d77d96ea113128e2f470c482423c
Drumil Bhati <64512394+drumilbhati@users.noreply.github.com>
2026-07-12 11:19:04 +0530

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sm = accumulate(nums.begin(), nums.end(), 0);
+
+        vector<int> dp(5001);
+        dp[0] = 1;
+
+        sort(nums.begin(), nums.end());
+
+        for (int x: nums) {
+            for (int i=5000; i>=x; i--) {
+                dp[i] += dp[i-x];
+            }
+        }
+
+        return (sm % 2 == 0 && dp[sm / 2]);
+    }
+};
